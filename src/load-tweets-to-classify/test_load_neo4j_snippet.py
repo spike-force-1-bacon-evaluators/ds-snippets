169b7c17274a1f1fa87b92ffa5d8bb33b5274eec
matheussilveira <matheus.silveira@marionete.co.uk>
2017-02-10 09:42:19 +0000

    fixed error in loading module

--- src/load-tweets-to-classify/test_load_neo4j_snippet.py
+++ src/load-tweets-to-classify/test_load_neo4j_snippet.py
@@ -1,4 +1,4 @@
-import loadTweets4SentimentAnalysis as lt4sa
+import load_neo4j_snippet as lt4sa
 
 def test_get_conn_string():
     assert lt4sa.get_conn_string() == "http://MYNEO4JUSER:MYNEO4JPASS@138.68.146.141:7474/db/data"
